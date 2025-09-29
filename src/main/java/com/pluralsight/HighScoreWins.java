61b381af60ddb7bdb43ed16eb2644dcdba22e98a
Tjones <taouris.jones@gmail.com>
2025-09-29 14:26:37 -0400

    commit

---
+++ src/main/java/com/pluralsight/HighScoreWins.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class HighScoreWins {
+    public static void main(String[] args) {
+        Scanner HSW = new Scanner(System.in);
+
+
+    }
+}
