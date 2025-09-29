324ed15682c486099ef880697b03311e5c014e64
Tjones <taouris.jones@gmail.com>
2025-09-29 15:11:41 -0400

    commit

--- src/main/java/com/pluralsight/HighScoreWins.java
+++ src/main/java/com/pluralsight/HighScoreWins.java
@@ -5,6 +5,24 @@ import java.util.Scanner;
 public class HighScoreWins {
     public static void main(String[] args) {
         Scanner HSW = new Scanner(System.in);
+        //Home:Visitor|21:9
+
+
+
+        String scoring = "Home|Visitor|21|9";
+
+        String[] splitInput = scoring.split("\\|");
+
+        String home = splitInput[0];
+        String visitor = splitInput[1];
+        int score1 = Integer.parseInt(splitInput[2]);
+        int score2 = Integer.parseInt(splitInput[3]);
+
+        System.out.println("And The Team With The Winning Score Is: %s, score, team ");
+
+
+
+
 
 
     }
