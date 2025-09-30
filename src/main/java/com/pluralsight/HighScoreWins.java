dd117a2934d2049fb7ce3c50c9502d2f7667f19b
Tjones <taouris.jones@gmail.com>
2025-09-30 10:17:26 -0400

    commit

--- src/main/java/com/pluralsight/HighScoreWins.java
+++ src/main/java/com/pluralsight/HighScoreWins.java
@@ -1,5 +1,7 @@
 package com.pluralsight;
 
+import com.sun.jdi.event.EventIterator;
+
 import java.util.Scanner;
 
 public class HighScoreWins {
@@ -9,16 +11,23 @@ public class HighScoreWins {
 
 
 
-        String scoring = "Home|Visitor|21|9";
+        String scoring = "BlueLock|AoAshi|21|9";
 
         String[] splitInput = scoring.split("\\|");
 
-        String home = splitInput[0];
-        String visitor = splitInput[1];
+        String blueLock = splitInput[0];
+        String aoAshi = splitInput[1];
         int score1 = Integer.parseInt(splitInput[2]);
         int score2 = Integer.parseInt(splitInput[3]);
 
-        System.out.println("And The Team With The Winning Score Is: %s, score, team ");
+        String winner;
+        if (score1 > score2)
+            winner = blueLock;
+        else
+            winner = aoAshi;
+
+
+        System.out.println("And The Team With The Winning Score Is:" + "\u001B[1m" + winner + "\u001B[0m");
 
 
 
